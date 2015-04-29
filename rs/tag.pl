5593bae47ce5c0344a8eb448eae657db5b7dcad8
7korobi <7korobi@gmail.com>
2015-04-29 18:15:52 +0900

    new character & format change.

---
+++ rs/tag.pl
@@ -0,0 +1,38 @@
+package SWResource_TAG;
+
+#----------------------------------------
+# ƒLƒƒƒ‰ƒNƒ^[ƒ^ƒO
+#----------------------------------------
+
+sub GetTag {
+  # ƒ^ƒO‚Ì–¼‘O
+  my %tag_name = (
+    'all' => '‚·‚×‚Ä',
+    'shoji' => '‚Ä‚â‚ñ‚Å‚¦',
+    'travel' => '‹AŠÒÒ‹c–',
+    'stratos' => '–¾Œã“ú‚Ö‚Ì“¹•W',
+    'myth' => '‚Í‚¨‚¤‚Ì‚Ğ‚ë‚Î',
+    'asia' => '‘å—¤‹c–',
+    'marchen' => '‹¶‘›‹c–',
+    'elegant' => '-•‘“¥‰ï-',
+    'guild' => '-¤H‰ï-',
+    'road' => '-‰^‰Í‰—ˆ-',
+    'apartment' => '-©º‚Ì‘‹-',
+    'farm' => '-X‚Ì”_ê-',
+    'servant' => '-g—pl-',
+    'medical' => '-{—Ã‰@-',
+    'market' => '-Š½ŠyŠXE•\\-',
+    'immoral' => '-Š½ŠyŠXE— -',
+    'law' => '-–@‚Ìx”z-',
+    'ecclesia' => '-Œö‹³‰ï-',
+    'god' => '-‚©‚İ‚³‚Ü-',
+  );  
+
+  my %tagset = (
+    TAG_NAME       => \%tag_name,
+  );
+
+  return \%tagset;
+}
+
+1;
